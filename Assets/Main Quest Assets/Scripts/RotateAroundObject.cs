9484d74df8c83fe51bf8b13d80c7dee9adac730a
Demis <demis_terborg@outlook.com>
2018-10-25 17:28:44 +0200

    add new scenes

---
+++ Assets/Main Quest Assets/Scripts/RotateAroundObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundObject : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+
+	void Start ()
+    {
+    }
+
+    void Update ()
+    {
+        transform.RotateAround(target.position, target.up, 20 * Time.deltaTime * speed);
+        //transform.LookAt(target);
+    }
+
+}
